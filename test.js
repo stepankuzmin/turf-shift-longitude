727ad80cdbf51e2d14a64a62371a13cf12bfec2f
Stepan Kuzmin <to.stepan.kuzmin@gmail.com>
2015-09-08 21:49:16 +0500

    update test.js

--- test.js
+++ test.js
@@ -8,13 +8,25 @@ test('shift', function (t) {
     "coordinates": [-175, 70]
   };
 
+  var linestring = {
+    "type": "LineString",
+    "coordinates": [[175,70], [-175,70]]
+  };
+
   t.deepEqual(shift(point),
     {
       "type": "Point",
       "coordinates": [185, 70]
     });
 
+  t.deepEqual(shift(linestring),
+    {
+      "type": "LineString",
+      "coordinates": [[175,70], [185,70]]
+    });
+
   t.deepEqual(point, point);
+  t.deepEqual(linestring, linestring);
 
   t.end();
 });
